791a47549efa13d6a1905ae05b243d68761cbb1e
kipe <tkrs23280810@gmail.com>
2020-03-01 15:35:54 +0900

    finish chapter5-3

--- shun.php
+++ shun.php
@@ -6,29 +6,29 @@
 </head>
 <body>
 
-	<?php
+  <?php
 
-    $tsuki = $_POST['tsuki'];
+  $tsuki = $_POST['tsuki'];
 
-    $yasai[] = '';
-    $yasai[] = 'ブロッコリー';
-    $yasai[] = 'カリフラワー';
-    $yasai[] = 'レタス';
-    $yasai[] = 'みつば';
-    $yasai[] = 'アスパラガス';
-    $yasai[] = 'セロリ';
-    $yasai[] = 'ナス';
-    $yasai[] = 'ピーマン';
-    $yasai[] = 'オクラ';
-    $yasai[] = 'さつまいも';
-    $yasai[] = '大根';
-    $yasai[] = 'ほうれんそう';
+  $yasai[] = '';
+  $yasai[] = 'ブロッコリー';
+  $yasai[] = 'カリフラワー';
+  $yasai[] = 'レタス';
+  $yasai[] = 'みつば';
+  $yasai[] = 'アスパラガス';
+  $yasai[] = 'セロリ';
+  $yasai[] = 'ナス';
+  $yasai[] = 'ピーマン';
+  $yasai[] = 'オクラ';
+  $yasai[] = 'さつまいも';
+  $yasai[] = '大根';
+  $yasai[] = 'ほうれんそう';
 
-    print $tsuki;
-    print '月は';
-    print $yasai[$tsuki];
-    print 'が旬です。';
-	?>
+  print $tsuki;
+  print '月は';
+  print $yasai[$tsuki];
+  print 'が旬です。';
+  ?>
 
 </body>
 </html>
